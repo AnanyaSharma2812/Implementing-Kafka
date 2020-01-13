d240a915c54bed2d824df7bca800699719acd211
Viharika Sai Sree Dhulipalla <viharikasaisreedhulipalla@Viharikas-MacBook-Pro.local>
2020-01-13 20:01:41 +0530

    3Commit

--- src/main/java/com/training/kafkademo/service/impl/CSVFileHandlerServicesImplementation.java
+++ src/main/java/com/training/kafkademo/service/impl/CSVFileHandlerServicesImplementation.java
@@ -4,13 +4,60 @@ import com.training.kafkademo.documentandentity.Employee;
 import com.training.kafkademo.service.MyFileHandlerServices;
 import org.springframework.stereotype.Service;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 
 @Service
 public class CSVFileHandlerServicesImplementation implements MyFileHandlerServices
 {
+    RandomAccessFile readFile;
+    static int counter=0;
+
+    {
+        try {
+            readFile=new RandomAccessFile("/Users/viharikasaisreedhulipalla/Downloads/employee.csv","rw");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     public Employee read() {
+        try {
+            readFile.seek(counter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Employee emp=new Employee();
+        DateFormat formatter = new SimpleDateFormat("MM/dd/yy");
+        try {
+            String line = readFile.readLine();
+            if (line != null) {
+                String[] members = line.split(",");
+                emp.setFirstName(members[0]);
+                emp.setLastName(members[1]);
+                emp.setDateOfBirth(formatter.parse(members[2]));
+                emp.setExperience(Double.parseDouble(members[3]));
+
+            }
+            counter=counter+line.length()+1;
+
+
+        }
+        catch (IOException |ParseException exception)
+        {
+            System.out.println(exception.getMessage());
+        }
+
+
         return null;
     }
 
